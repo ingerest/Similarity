540e2ac0e5056c8166e77c9043b8fa21e1fe8cfc
Sehun Oh <ingerest1@naver.com>
2025-03-28 09:58:33 +0900

    Same Length

--- Project27/similarity.cpp
+++ Project27/similarity.cpp
@@ -1,5 +1,15 @@
+#include <string>
+
 class Similarity
 {
 public:
+	int getSimilarityScore(std::string str1, std::string str2)
+	{
+		if (str1.length() == str2.length())
+		{
+			return 60;
+		}
 
+		return 0;
+	}
 };
--- Project27/similarity_test.cpp
+++ Project27/similarity_test.cpp
@@ -1,4 +1,13 @@
 #include "gmock/gmock.h"
+#include "similarity.cpp"
+
+
+TEST(TestGroup, TestSameLength)
+{
+	Similarity similarity;
+	int result = similarity.getSimilarityScore("ASD", "DSA");
+	EXPECT_EQ(60, result);
+}
 
 int main()
 {
