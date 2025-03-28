b70ada5326ff0ef7ca4342f2e58952f896c7dc05
Sehun Oh <ingerest1@naver.com>
2025-03-28 13:11:00 +0900

    alpha similarity

--- Project27/similarity.cpp
+++ Project27/similarity.cpp
@@ -1,9 +1,10 @@
 #include <string>
+#include <set>
 
 class Similarity
 {
 public:
-	int getSimilarityScore(std::string str1, std::string str2)
+	int getSimilarityLengthScore(std::string str1, std::string str2)
 	{
 		int big = str1.length();
 		int small = str2.length();
@@ -21,5 +22,30 @@ public:
 		
 		int gap = big - small;
 		return (1 - (double)gap / (double)small) * 60;
+	}
+
+	int getSimilarityAlphaScore(std::string str1, std::string str2)
+	{
+		std::set<char> set1, set2;
+
+		for (char ch : str1) {
+			if (isupper(ch)) set1.insert(ch);
+		}
+		for (char ch : str2) {
+			if (isupper(ch)) set2.insert(ch);
+		}
+
+		int sameCnt = 0;
+		for (char ch : set1) {
+			if (set2.count(ch)) sameCnt++;
+		}
+
+		std::set<char> unionSet = set1;
+		unionSet.insert(set2.begin(), set2.end());
+		int totalCnt = unionSet.size();
+
+		if (totalCnt == 0) return 0;
+
+		return ((double)sameCnt / (double)totalCnt) * 40;
 	}
 };
